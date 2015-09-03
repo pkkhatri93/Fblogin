099338dd5b355e02b56115a2d4ff6da10ea56d8c
pkkhatri93 <jvashisht@COG-MAC-003.local>
2015-09-03 17:35:15 +0530

    k
    k

--- Fblogin/AppDelegate.m
+++ Fblogin/AppDelegate.m
@@ -34,7 +34,11 @@
                                                        annotation:annotation];
 //tesing the new fun
     NSLog(@"worki9ng");
-   
+     NSLog(@"work from myside");
+    NSLog(@"work from myside");
+    NSLog(@"work from myside");
+    NSLog(@"work from myside");  
+
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
